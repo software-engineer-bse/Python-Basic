ecb32fcc8c0b6a8f20c2b8290af0075b66834072
software-engineer-bse <khurram.email.123@gmail.com>
2024-08-21 05:26:42 -0700

    Add Exercise 001 Write a script that take user input for a number then adds 3 to that number. Then multiplies the result by 2, subtract 4, then again adds 3, then print the result.py

---
+++ Exercises/001 Write a script that take user input for a number then adds 3 to that number. Then multiplies the result by 2, subtract 4, then again adds 3, then print the result..py
@@ -0,0 +1,15 @@
+"""
+Write a script that take user input for a number then adds 3 to that number. Then multiplies the result by 2, subtract 4, then again adds 3, then print the result.
+"""
+
+number = int(input("Enter a number: "))
+
+result = number + 3
+
+result = result * 2
+
+result = result - 4
+
+result = result + 3
+
+print("The result is:", result)
