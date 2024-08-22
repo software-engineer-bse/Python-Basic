ebf93980efb1d5c87938417dc40f53a8816fc9bb
software-engineer-bse <khurram.email.123@gmail.com>
2024-08-22 07:11:56 -0700

    Add Exercise 002 Write a script that takes input as Celsius and then convert Celsius to Fahrenheit.py file

---
+++ Exercises/002 Write a script that takes input as Celsius and then convert Celsius to Fahrenheit.py
@@ -0,0 +1,9 @@
+"""
+Write a script that takes input as Celsius and then convert Celsius to Fahrenheit.
+(hint: Fahrenheit = (Celsius * 1.8) + 32)
+"""
+celsius = float(input("Enter celsius: "))
+
+fahrenheit = (celsius * 1.8) + 32
+
+print("The temperature is:", fahrenheit, "fahrenheit")
