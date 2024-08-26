f30bfa15d9e1b37735f775911a07e3a79dfefc1c
software-engineer-bse <khurram.email.123@gmail.com>
2024-08-26 03:34:01 -0700

    003 Write a script that takes input as radius then calculate area of circle.py

---
+++ Exercises/003 Write a script that takes input as radius then calculate area of circle.py
@@ -0,0 +1,6 @@
+"""
+Write a script that takes input as radius then calculate area of circle. (hint: A = πr²)
+"""
+radius = float(input("Enter radius: "))
+A = 3.14 * (radius**2)
+print(f"Area of circle is: {A}")
