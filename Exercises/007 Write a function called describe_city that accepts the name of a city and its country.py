5e530272ddf48d675e637772ee99252e7b8f051a
software-engineer-bse <khurram.email.123@gmail.com>
2024-09-03 11:17:26 -0700

    Add Exercise 007 Write a function called describe_city that accepts the name of a city and its country.py

---
+++ Exercises/007 Write a function called describe_city that accepts the name of a city and its country.py
@@ -0,0 +1,9 @@
+"""
+Write a function called describe_city that accepts the name of a city and its
+country.
+"""
+def describe_city(city, country):
+    print(f"{city} is the city of {country}.")
+
+
+describe_city("Karachi", "Pakistan")
