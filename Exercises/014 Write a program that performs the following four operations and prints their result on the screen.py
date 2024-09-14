f51096b8099a1754298668ff930f13149bdc927f
software-engineer-bse <khurram.email.123@gmail.com>
2024-09-14 09:33:56 -0700

    Add Exercise 014 Write a program that performs the following four operations and prints their result on the screen.py

---
+++ Exercises/014 Write a program that performs the following four operations and prints their result on the screen.py
@@ -0,0 +1,12 @@
+"""
+Write a program that performs the following four operations and prints their result on
+the screen.
+a. 50 + 4
+b. 50 - 4
+c. 50 * 4
+d. 50 / 4
+"""
+print(50 + 4)
+print(50 - 4)
+print(50 * 4)
+print(50 / 4)
