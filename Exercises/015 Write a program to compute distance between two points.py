faf2724f8c58fb041b9b26c7d07063742e5b4018
software-engineer-bse <khurram.email.123@gmail.com>
2024-09-15 14:28:58 -0700

    Add Exercise 015 Write a program to compute distance between two points.py

---
+++ Exercises/015 Write a program to compute distance between two points.py
@@ -0,0 +1,13 @@
+"""
+Write a program to compute distance between two points by creating variables
+(Pythagorean Theorem)
+Distance =((x2-x1)^2+(y2-y1)^2)^1/2
+"""
+x1 = 2
+x2 = 2
+
+y1 = 4
+y2 = 1
+
+distance = (((x2-x1)**2)+((y2-y1)**2))**(1/2)
+print(distance)
