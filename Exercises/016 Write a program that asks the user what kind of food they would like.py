d0d88d65f59c7012529d77ce7ce232de7a814000
software-engineer-bse <khurram.email.123@gmail.com>
2024-09-16 09:00:36 -0700

    Add Exercise 016 Write a program that asks the user what kind of food they would like.py

---
+++ Exercises/016 Write a program that asks the user what kind of food they would like.py
@@ -0,0 +1,7 @@
+"""
+Write a program that asks the user what kind of food they would like. Print a message about that
+food, such as “Let me see if I can find you a Chowmein”. Food name must be in uppercase. (hint:
+use upper( ) for food name)
+"""
+FAV_FOOD = input("WHAT KIND OF FOOD YOU LIKE? ")
+print("LET ME SEE IF I CAN FIND YOU A" , FAV_FOOD.upper())
